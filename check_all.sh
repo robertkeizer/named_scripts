eaa108dff9e3c4bcaf0e29bf5b97ef21785caf8b
Robert <robert@keizer.ca>
2011-05-28 16:01:10 -0500

    Finished restart script.

---
+++ check_all.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+vars="`echo $0 | sed 's/check_all.sh/vars/'`";
+source $vars;
+
+# Check the zones..
+for master_slave in $MASTER_SLAVE; do
+	for location in `ls $NAMED_PATH/$master_slave | egrep "$LOCATION_REGEX"`; do
+		for file in `ls $NAMED_PATH/$master_slave/$location/ | egrep "$FILE_REGEX"`; do
+			domain="`echo $file | sed 's/\.zone\.db$//'`";
+
+			echo -n "$location: $domain ... ";
+			named-checkzone -t $NAMED_PATH $domain /$master_slave/$location/$file >/dev/null 2>&1
+			if [ $? -ne 0 ]; then
+				echo " failed.";
+				exit 1;
+			else
+				echo " okay.";
+			fi;
+		done;
+	done;
+done;
+
+echo -n "named config ...";
+named-checkconf -t /var/named >/dev/null 2>&1;
+if [ $? -ne 0 ]; then
+	echo " failed.";
+	exit 1;
+else
+	echo " okay.";
+fi;
--- restart_named.sh
+++ restart_named.sh
@@ -1,19 +1,49 @@
 #!/usr/bin/env bash
-BINPATH="`echo $0 | sed 's/\/restart_named.sh//'`";
-source "$BINPATH/vars";
+NAMED_SCRIPTS_ROOT="`echo $0 | sed 's/restart_named.sh//'`";
+source $NAMED_SCRIPTS_ROOT/vars;
 
-tmp_output="`$BINPATH/verify_all.sh`";
-if [ "`echo "$tmp_output" | tail -1`" == "OK" ]; then
-	echo "$tmp_output";
-	# Reload the name server.
-	pkill -HUP named;
-	# Copy to other server and reload it too.
-	# /usr/bin/scp -r $NAMED_PATH root@foobar:/var/named/
-	# /usr/bin/ssh root@foobar -C "pkill -HUP named"
-	echo "Restarted named.";
-	echo "OK";
-else
-	echo "There were errors";
-	echo "$tmp_output";
+$NAMED_SCRIPTS_ROOT/check_all.sh
+if [ $? -ne 0 ]; then
+	echo "Will not restart named since you have errors.";
 	exit 1;
+fi;
+
+for master_slave in $MASTER_SLAVE; do
+	for location in `ls $NAMED_PATH/$master_slave | egrep "$LOCATION_REGEX"`; do
+		echo -n "Transferring: $location ...";
+		scp -P$REMOTE_PORT -i$REMOTE_KEY -r $NAMED_PATH/$master_slave/$location $REMOTE_USER@$REMOTE_SERVER:$NAMED_PATH/$master_slave >/dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			echo " failed.";
+			exit 1;
+		else
+			echo " okay.";
+		fi;
+	done;
+done;
+
+echo -n "Transferring: $NAMED_PATH/etc ...";
+scp -P$REMOTE_PORT -i$REMOTE_KEY -r $NAMED_PATH/etc $REMOTE_USER@$REMOTE_SERVER:$NAMED_PATH/ >/dev/null 2>&1;
+if [ $? -ne 0 ]; then
+	echo " failed.";
+	exit 1;
+else
+	echo " okay.";
+fi;
+
+echo -n "Reloading local ...";
+pkill -HUP named;
+if [ $? -ne 0 ]; then
+	echo " failed.";
+	exit 1;
+else
+	echo " okay.";
+fi;
+
+echo -n "Reloading remote ...";
+ssh -p$REMOTE_PORT -i$REMOTE_KEY $REMOTE_USER@$REMOTE_SERVER -C "pkill -HUP named";
+if [ $? -ne 0 ]; then
+	echo " failed.";
+	exit 1;
+else
+	echo " okay.";
 fi;
